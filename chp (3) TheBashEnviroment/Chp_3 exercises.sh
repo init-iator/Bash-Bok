d12e8a011fa8da875dfdeb3d903b82e594776346
init-iator <emuhnas1@hotmail.com>
2024-12-05 14:02:04 +0100

    init 2

--- chp (3) TheBashEnviroment/Chp_3 exercises.sh
+++ chp (3) TheBashEnviroment/Chp_3 exercises.sh
@@ -1,21 +1,76 @@
-#!/usr/bin/bash
+1.  Create 3 variables, VAR1, VAR2 and VAR3; initialize them to hold the values "thirteen", "13" and \
+    "Happy Birthday" respectively
+        $ var1=thirteen
+        $ var2=13
+        $ var3=Happy Birthday
+        $ echo $var1 $var2 $var3
+        thirteen 13 Happy Birthday
 
-# This script containes exercises for chapter 3
+3.  Are these local or global variables?\
+        Global variables
 
-# Declare and set variables
-VAR1="thirteen"
-VAR2="13"
-VAR3="Happy Birthday"
+4.  Remove var3
+        $ unset var3
 
-# Print variables
-echo $VAR1 $VAR2 $VAR3
+(6. 7. 8.)
+    1. Edit /etc/profile for Greeting Users
+        Add a greeting message for all users:
+            # Display a greeting message to all users
+            echo "Welcome, $USER! You are logged in to $(hostname) on $(date)."
 
-# Test if vars are global
-typeset -p VAR1 VAR2 VAR3
+        Customize prompt for root account:
+            # Custom prompt for root
+            if [ "$USER" == "root" ]; then
+                PS1='\[\e[1;41m\]Danger!! root is doing stuff in \w\[\e[0m\] > '
+            else
+                PS1='\u@\h \w> '  # Default prompt for all users
+            fi
 
-# Remove VAR1
-unset VAR1
+    2. Test the Changes
+        source /etc/profile
 
-# Print again
+    3. Optional: Modify Prompt for Specific Users
 
-echo $VAR1 $VAR2 $VAR3
+        if [ "$USER" == "root" ]; then
+            PS1='\[\e[1;41m\]Danger!! root is doing stuff in \w\[\e[0m\] > '
+        elif [ "$USER" == "newuser" ]; then
+            PS1='\[\e[1;32m\]Welcome, newuser! You are in \w\[\e[0m\] > '
+        else
+            PS1='\u@\h \w> '
+        fi
+
+9.
+    nano rectangle_area.sh
+
+    #!/bin/bash
+
+    # rectangle_area.sh - A script to calculate the surface area of a rectangle.
+    # It assigns values to two integer variables representing length and width,
+    # calculates the surface area, and displays the result in a readable format.
+
+    # Assigning integer values to the variables for length and width
+    length=10
+    width=5
+
+    # Calculating the surface area of the rectangle
+    area=$((length * width))
+
+    # Generating elegant output to show the result
+    echo "========================================"
+    echo " Surface Area Calculation of a Rectangle"
+    echo "========================================"
+    echo "Length    : $length units"
+    echo "Width     : $width units"
+    echo "----------------------------------------"
+    echo "Area      : $area square units"
+    echo "========================================"
+
+    # End of script
+
+
+
+
+
+
+
+
